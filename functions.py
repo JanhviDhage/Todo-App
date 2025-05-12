0f9dffdfec17a6380987a934ea9e26161dbaa90b
JanhviDhage <janhavidhage1609@gmail.com>
2025-05-12 19:21:02 +0530

    Initial commit

---
+++ functions.py
@@ -0,0 +1,9 @@
+FILEPATH="todo.txt"
+def get_todos(filepath=FILEPATH):
+    with open(filepath,"r") as file_local:
+        todo_local=file_local.readlines()
+    return todo_local
+
+def write_todos(todos_arg,filepath=FILEPATH):
+    with open(filepath,"w") as file:
+        file.writelines(todos_arg)
